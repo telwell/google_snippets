5a23d0fb7767aab123729b01cc6b0ffd1e81b803
telwell <me@trevorelwell.com>
2015-11-07 17:33:22 -0500

    bug fix for heroku in UserMailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,7 +7,7 @@ class UserMailer < ApplicationMailer
     @snippets = @project.snippets.where('created_at >= ?', 1.week.ago)
 
     # Using a Proc to send to ALL subscribers for that particular project
-    mail(	to: to: Proc.new { @subscribers.pluck(:email) }, 
+    mail(	to: Proc.new { @subscribers.pluck(:email) }, 
     			subject: "Weekly Snippet Digest Email | #{@project.name.titleize}")
 
   end
